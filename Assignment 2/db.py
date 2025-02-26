4cd70d59ac4946213933cfdbf4f26a987312fb03
Mohammad Bayu Rizki <widjanarko020@gmail.com>
2025-02-26 14:25:52 +0700

    update 3.0
    update baru

--- Assignment 2/db.py
+++ Assignment 2/db.py
@@ -1,13 +1,19 @@
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-client = MongoClient("mongodb+srv://405Found-sic6:o4qL4fslJVAsoomm@405found.9ibtc.mongodb.net/?appName=405Found")
+uri = "mongodb+srv://admin-405found:PNOJHpdexffLWupu@405found.9ibtc.mongodb.net/?retryWrites=true&w=majority&appName=405Found"
 
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
-    
+
+# client = MongoClient("mongodb+srv://405Found-sic6:PNOJHpdexffLWupu@405found.9ibtc.mongodb.net/?appName=405Found")
+
     
 db = client["MyDatabases"]  
 collection = db["SensorData"]  
